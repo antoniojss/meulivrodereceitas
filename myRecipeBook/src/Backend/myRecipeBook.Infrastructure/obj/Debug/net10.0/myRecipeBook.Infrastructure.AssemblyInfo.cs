5d0adc907dc7b36d353a11ab4809850916ed39f1
antoniojss <antoniojss@msn.com>
2026-07-25 17:24:29 -0300

    apaguei um linja em branco no arquyivo de solução

--- myRecipeBook/src/Backend/myRecipeBook.Infrastructure/obj/Debug/net10.0/myRecipeBook.Infrastructure.AssemblyInfo.cs
+++ myRecipeBook/src/Backend/myRecipeBook.Infrastructure/obj/Debug/net10.0/myRecipeBook.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myRecipeBook.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+011cffe931a1baa1f6f0fbbcace3d5499c2714e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54c5115c43ff84c0a4b547b5bc79e7f33c0ca0e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("myRecipeBook.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myRecipeBook.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
