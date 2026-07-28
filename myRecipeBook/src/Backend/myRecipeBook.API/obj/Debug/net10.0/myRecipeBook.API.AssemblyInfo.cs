37c25558bf5d17a0ff4774e17bc6b692df7397e4
antoniojss <antoniojss@msn.com>
2026-07-27 22:07:43 -0300

    enviar o arquivo de coverlat

--- myRecipeBook/src/Backend/myRecipeBook.API/obj/Debug/net10.0/myRecipeBook.API.AssemblyInfo.cs
+++ myRecipeBook/src/Backend/myRecipeBook.API/obj/Debug/net10.0/myRecipeBook.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myRecipeBook.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d0adc907dc7b36d353a11ab4809850916ed39f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4668d2e838076dcfba27afd714ca5382f7d43ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("myRecipeBook.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myRecipeBook.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
